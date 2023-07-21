54145095134d51848de5efd58beecdba2f956ef4
Sergio Vidales Perez <srvidales@gmail.com>
2023-07-21 16:54:45 -0700

    Adds all app sections

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,3 +28,14 @@ await getPlanets();
 
 console.log(people);
 console.log(planets);
+
+// Step 1 - Introduction
+// Step 2 - Pick universe - Multiple universes in version 2?
+// Step 3 - Select characters in that universe
+// Step 4 - Select locations/planets in selected universe
+// Step 5 - Plot twists? Number of critical points?
+// Step 6 - Generate story
+// Step 7 - Play through choices (Save Story)
+// Step 8 - Story finishes. Success/Failure.
+// Step 9 - Results (Save Results)
+
