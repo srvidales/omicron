5e8d9ce57704b9dd4bf753920d989eef2e240042
Sergio Vidales Perez <srvidales@gmail.com>
2023-07-21 02:23:15 -0700

    Adds getPeople and getPlanets

--- assets/js/api-1.js
+++ assets/js/api-1.js
@@ -4,12 +4,10 @@ export const functionA = function () {
 
 export const fetchPerson = async function (id) {
     const response = await fetch(`https://swapi.dev/api/people/${id}`);
-    const person = await response.json();
-    console.log(person);
+    return await response.json();
 }
 
 export const fetchPlanet = async function (id) {
     const response = await fetch(`https://swapi.dev/api/planets/${id}`);
-    const planet = await response.json();
-    console.log(planet)
+    return await response.json();
 }
--- assets/js/script.js
+++ assets/js/script.js
@@ -5,5 +5,26 @@ console.log('Hello, world!');
 
 functionA();
 functionB();
-fetchPerson(1).then(function () { console.log('Got a person!')});
-fetchPlanet(1).then(function () { console.log('Got a planet!')});
+
+let people = [];
+let planets = [];
+
+async function getPeople() {
+    for (let i = 0; i < 10; i++) {
+        const person = await fetchPerson(i + 1);
+        people.push(person);
+    }
+}
+
+async function getPlanets() {
+    for (let i = 0; i < 10; i++) {
+        const planet = await fetchPlanet(i + 1);
+        planets.push(planet);
+    }
+}
+
+await getPeople();
+await getPlanets();
+
+console.log(people);
+console.log(planets);
