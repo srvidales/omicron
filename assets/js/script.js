860ebb542a5a37c48b8bcf4d1d86cc72e6068452
Sergio Vidales Perez <srvidales@gmail.com>
2023-07-21 22:06:03 -0700

    Removes sample code in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,3 @@
-import {functionA, fetchPerson, fetchPlanet} from "./api-1.js";
-import functionB from './api-2.js';
-
-console.log('Hello, world!');
-
-functionA();
-functionB();
-
-let people = [];
-let planets = [];
-
-async function getPeople() {
-    for (let i = 0; i < 10; i++) {
-        const person = await fetchPerson(i + 1);
-        people.push(person);
-    }
-}
-
-async function getPlanets() {
-    for (let i = 0; i < 10; i++) {
-        const planet = await fetchPlanet(i + 1);
-        planets.push(planet);
-    }
-}
-
-await getPeople();
-await getPlanets();
-
-console.log(people);
-console.log(planets);
-
 // Step 1 - Introduction
 // Step 2 - Pick universe - Multiple universes in version 2?
 // Step 3 - Select characters in that universe
