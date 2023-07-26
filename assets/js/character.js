cd00b327dd8697772c7a957e0fd63776a883bb45
Dre Tay <andretaylor681@gmail.com>
2023-07-25 21:29:14 -0400

    test function

--- assets/js/character.js
+++ assets/js/character.js
@@ -6,3 +6,26 @@ const initCharacterStep = function () {
 }
 
 export {initCharacterStep}
+
+var randomCharacterBtn = document.getElementById("add-character-btn");
+var nextbtn = document.getElementById("continue-btn");
+console.log(randomCharacterBtn);
+
+function genRandomCharacter(){
+    var number = ['1','2','3','4','5','6','7','8','9','10'];
+    var ranNumber = number[Math.floor(Math.random() *number.length)];
+     
+    
+    var requestUrl ='https://swapi.dev/api/people/'+ ranNumber;
+    fetch(requestUrl)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data)
+
+    })
+    
+};
+
+randomCharacterBtn.addEventListener('click',genRandomCharacter);
