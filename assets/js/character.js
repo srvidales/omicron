dc8bfb9ad159a38a899fdc8e62a7a727c299a62c
Dre Tay <andretaylor681@gmail.com>
2023-07-25 21:56:29 -0400

    moved var randomCharacterbtn into the initCharacterStep function

--- assets/js/character.js
+++ assets/js/character.js
@@ -3,17 +3,18 @@ import {nextStep} from './app.js';
 const initCharacterStep = function () {
     console.log('Done loading Character step.')
     $('#character-div #continue-btn').on('click', nextStep)
+
+    var randomCharacterBtn = document.getElementById("add-character-btn");
     randomCharacterBtn.addEventListener('click',genRandomCharacter);
 }
 
-var randomCharacterBtn = document.getElementById("add-character-btn");
+
 var nextbtn = document.getElementById("continue-btn");
-console.log(randomCharacterBtn);
 
 function genRandomCharacter(){
     var number = ['1','2','3','4','5','6','7','8','9','10'];
     var ranNumber = number[Math.floor(Math.random() *number.length)];
-
+    console.log(ranNumber)
 
     var requestUrl ='https://swapi.dev/api/people/'+ ranNumber;
     fetch(requestUrl)
