7cf72a22aa02e34468870c06ef04864f72157add
Dre Tay <andretaylor681@gmail.com>
2023-07-28 00:03:13 -0400

    added code to push the data into an array for the character and location pages. after the data is pushed to an array i pinpoint the text areas in the document and changed the content of the text area to match the data.

--- assets/js/character.js
+++ assets/js/character.js
@@ -20,26 +20,46 @@ async function genRandomCharacter() {
   var ranNumber = number[Math.floor(Math.random() * number.length)];
   console.log(ranNumber);
 
-var homeWorldLink;
-var birthPlace;
-var eyeColor;
-var gender;
-var hairColor;
-var height;
+  var homeWorldLink;
+  var name;
+  var birthPlace;
+  var eyeColor;
+  var gender;
+  var hairColor;
+  var height;
+  var characterDataArray = [];
 
   var requestUrl = "https://swapi.dev/api/people/" + ranNumber;
   let response = await fetch(requestUrl);
   let data = await response.json();
   homeWorldLink = data.homeworld;
+  name = data.name;
   eyeColor = data.eye_color;
   gender = data.gender;
   hairColor = data.hair_color;
   height = data.height;
+  console.log(name);
   console.log(eyeColor);
-  console.log(data);
   console.log(gender);
   console.log(hairColor);
   console.log(height);
+
+  characterDataArray.push(name);
+  characterDataArray.push(eyeColor);
+  characterDataArray.push(gender);
+  characterDataArray.push(hairColor);
+  characterDataArray.push(height);
+
+  var names = document.getElementById("name");
+  names.textContent = characterDataArray[0];
+  var eyes = document.getElementById("eyeColor");
+  eyes.textContent = characterDataArray[1];
+  var genders = document.getElementById("gender");
+  genders.textContent = characterDataArray[2];
+  var hair = document.getElementById("hairColor");
+  hair.textContent = characterDataArray[3];
+  var heights = document.getElementById("height");
+  heights.textContent = characterDataArray[4];
 
   async function fetchHomeWorld() {
     console.log(homeWorldLink);
@@ -48,11 +68,14 @@ var height;
     console.log(data);
     birthPlace = data.name;
     console.log(birthPlace);
-  
+
+    characterDataArray.push(birthPlace);
+
+    var placeOfBirth = document.getElementById("birthPlace");
+    placeOfBirth.textContent = characterDataArray[5];
   }
 
   fetchHomeWorld();
-  
 }
 // async function fetchHomeWorld() {
 //   console.log(homeWorldLink);
@@ -63,6 +86,5 @@ var height;
 //   console.log(birthPlace);
 //   return data;
 // }
-
 
 export { initCharacterStep };
